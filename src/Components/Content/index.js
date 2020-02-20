bf17c45fb63a5f44fab5db309ecf389e7001a521
James Mudidi <mudidi.jimmy@gmail.com>
2020-02-20 14:27:51 +0300

    add more links

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -1,33 +1,52 @@
 import React, { Component } from 'react';
 import { Card, CardColumns, Button, Row, Col } from 'react-bootstrap';
+import { css } from "@emotion/core";
+import CircleLoader from "react-spinners/CircleLoader";
 
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 class Content extends Component {
-	state = {
-		articles: []
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+			articles: []
+		};
 	}
 
 	componentDidMount() {
 		fetch('https://newsapi.org/v2/top-headlines?language=en&apiKey=ee3998a05fc34b3e9f25cce1509dbfd2')
 			.then(res => res.json())
 			.then((data) => {
-				this.setState({ articles: data.articles })
+				this.setState({ loading: false, articles: data.articles })
 				console.log(this.state.articles)
 			})
 			.catch(console.log)
 	}
 
 	render() {
-
 		return (
-			<div>
-				<div className="container">
-					<h1>Headlines</h1>
+			<div className="container">
+				<h1>Headlines</h1>
+				<div>
+					<div className="sweet-loading">
+						<CircleLoader
+							css={override}
+							size={150}
+							color={"#007BFF"}
+							loading={this.state.loading}
+						/>
+					</div>
 					<CardColumns>
 						{this.state.articles.map((articles) => (
 							<Row>
-								<Col xs={12}>
-									<Card keys={Object.keys(articles)} xs={3}>
+								<Col xl={12}>
+									<Card keys={Object.keys(articles)}>
 										<Card.Img variant="top" src={articles.urlToImage} />
 										<Card.Body>
 											<Card.Title>{articles.title}</Card.Title>
